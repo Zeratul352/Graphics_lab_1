4182c192f1d9a9991495f69f1f9ccc57a27d2e3b
Zeratul352 <43727651+Zeratul352@users.noreply.github.com>
2020-09-22 19:15:21 +0300

    Initial commit

---
+++ Graphics_lab_1/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Graphics_lab_1
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.TryDraw = new System.Windows.Forms.Button();
+            this.DrawGrid = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.Ignore = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openfile = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Clear = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picture
+            // 
+            this.Picture.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Picture.Location = new System.Drawing.Point(13, 13);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(600, 600);
+            this.Picture.TabIndex = 0;
+            this.Picture.TabStop = false;
+            this.Picture.Click += new System.EventHandler(this.Picture_Click);
+            this.Picture.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Picture_MouseClick);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // TryDraw
+            // 
+            this.TryDraw.Location = new System.Drawing.Point(633, 133);
+            this.TryDraw.Name = "TryDraw";
+            this.TryDraw.Size = new System.Drawing.Size(126, 38);
+            this.TryDraw.TabIndex = 1;
+            this.TryDraw.Text = "Draw";
+            this.TryDraw.UseVisualStyleBackColor = true;
+            this.TryDraw.Click += new System.EventHandler(this.TryDraw_Click);
+            // 
+            // DrawGrid
+            // 
+            this.DrawGrid.Location = new System.Drawing.Point(633, 177);
+            this.DrawGrid.Name = "DrawGrid";
+            this.DrawGrid.Size = new System.Drawing.Size(126, 39);
+            this.DrawGrid.TabIndex = 2;
+            this.DrawGrid.Text = "Draw grid";
+            this.DrawGrid.UseVisualStyleBackColor = true;
+            this.DrawGrid.Click += new System.EventHandler(this.DrawGrid_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(633, 222);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(126, 37);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Nextiteration";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Ignore
+            // 
+            this.Ignore.Location = new System.Drawing.Point(633, 265);
+            this.Ignore.Name = "Ignore";
+            this.Ignore.Size = new System.Drawing.Size(126, 36);
+            this.Ignore.TabIndex = 4;
+            this.Ignore.Text = "Ignore boarder";
+            this.Ignore.UseVisualStyleBackColor = true;
+            this.Ignore.Click += new System.EventHandler(this.Ignore_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openfile
+            // 
+            this.openfile.Location = new System.Drawing.Point(633, 307);
+            this.openfile.Name = "openfile";
+            this.openfile.Size = new System.Drawing.Size(126, 32);
+            this.openfile.TabIndex = 5;
+            this.openfile.Text = "Openrule";
+            this.openfile.UseVisualStyleBackColor = true;
+            this.openfile.Click += new System.EventHandler(this.openfile_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(633, 389);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(126, 51);
+            this.save.TabIndex = 6;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "bmp";
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(633, 345);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(126, 38);
+            this.Clear.TabIndex = 7;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "5",
+            "6",
+            "10",
+            "15",
+            "20",
+            "30",
+            "50",
+            "100"});
+            this.comboBox1.Location = new System.Drawing.Point(635, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(124, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(632, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Pixels per square";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(631, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Iterations per tick";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "5",
+            "10",
+            "20",
+            "50",
+            "100",
+            "200",
+            "300",
+            "500",
+            "1000"});
+            this.comboBox2.Location = new System.Drawing.Point(635, 84);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(124, 24);
+            this.comboBox2.TabIndex = 11;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(774, 633);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.openfile);
+            this.Controls.Add(this.Ignore);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.DrawGrid);
+            this.Controls.Add(this.TryDraw);
+            this.Controls.Add(this.Picture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button TryDraw;
+        private System.Windows.Forms.Button DrawGrid;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button Ignore;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openfile;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Graphics_lab_1/Form1.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics_lab_1
+{
+    public partial class MainWindow : Form
+    {
+        int[,] StatusGrid;
+        Dictionary<Tuple<int, int>, int> extrapoints;
+        bool ignoreboarderflag = false;
+        bool gridflag = false;
+        static int step = 3;
+        static int count = 600 / step;
+        int termitex = count/ 2 - 1;
+        int termitey = count/ 2 - 1;
+        int tickcount = 300;
+        char termitestate = 'A';
+        List<Tuple<int, int>> directions;
+        int currentdirection = 0;
+        Random random = new Random();
+        List<Color> brushes = new List<Color>();
+        Dictionary<Tuple<char, int>, Tuple<int, int, char>> rule;
+        static Bitmap map = new Bitmap(count * step, count * step);
+        string filename = "Default";
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            StatusGrid = new int[count, count];
+            for(int i = 0; i < count; i++)
+            {
+                for(int j = 0; j < count; j++)
+                {
+                    StatusGrid[i, j] = 0;
+                }
+            }
+            //adding colors to palitre
+            brushes.Add(Color.Black);
+            brushes.Add(Color.White);
+            brushes.Add(Color.Red);
+            brushes.Add(Color.Lime);
+            brushes.Add(Color.Blue);
+            brushes.Add(Color.Yellow);
+            brushes.Add(Color.Aqua);
+            brushes.Add(Color.Magenta);
+            brushes.Add(Color.Silver);
+            brushes.Add(Color.Gray);
+            brushes.Add(Color.Maroon);
+            brushes.Add(Color.Olive);
+            brushes.Add(Color.Green);
+            brushes.Add(Color.Purple);
+            brushes.Add(Color.Teal);
+            brushes.Add(Color.Navy);
+            //adding rule
+            rule = new Dictionary<Tuple<char, int>, Tuple<int, int, char>>();
+            rule.Add(new Tuple<char, int>('A', 0), new Tuple<int, int, char>(12, 0, 'C'));
+            rule.Add(new Tuple<char, int>('A', 12), new Tuple<int, int, char>(0, 0, 'B'));
+            rule.Add(new Tuple<char, int>('B', 12), new Tuple<int, int, char>(12, 1, 'A'));
+            rule.Add(new Tuple<char, int>('B', 3), new Tuple<int, int, char>(12, 1, 'A'));
+            rule.Add(new Tuple<char, int>('C', 12), new Tuple<int, int, char>(0, -1, 'A'));
+            rule.Add(new Tuple<char, int>('C', 0), new Tuple<int, int, char>(3, -1, 'A'));
+            rule.Add(new Tuple<char, int>('C', 3), new Tuple<int, int, char>(12, -1, 'A'));
+            //
+            directions = new List<Tuple<int, int>>();
+            directions.Add(new Tuple<int, int>(0, 1));
+            directions.Add(new Tuple<int, int>(1, 0));
+            directions.Add(new Tuple<int, int>(0, -1));
+            directions.Add(new Tuple<int, int>(-1, 0));
+            //map.SetResolution(16, 1600);
+            //Picture.CreateGraphics();
+            //saveFileDialog1.Filter = "Text files"
+        }
+        private void Init()
+        {
+            timer.Stop();
+            comboBox1.Enabled = true;
+            StopButton.Enabled = true;
+            save.Enabled = true;
+            openfile.Enabled = true;
+            TryDraw.Text = "Draw";
+            DrawGrid.Text = "Draw grid";
+            count = 600 / step;
+            StatusGrid = new int[count, count];
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    StatusGrid[i, j] = 0;
+                }
+            }
+            map = new Bitmap(step * count, step * count);
+            termitex = count / 2 - 1;
+            termitey = count / 2 - 1;
+            termitestate = 'A';
+            //List<Tuple<int, int>> directions;
+            currentdirection = 0;
+            Picture.Image = map;
+            ignoreboarderflag = false;
+            gridflag = false;
+            Ignore.Text = "Ignore boarder";
+            extrapoints = null;
+            //extrapoints = new Dictionary<Tuple<int, int>, int>();
+        }
+        private void TryDraw_Click(object sender, EventArgs e)
+        {
+            Graphics drawer = Picture.CreateGraphics();
+            if (timer.Enabled)
+            {
+                TryDraw.Text = "Draw";
+                timer.Stop();
+                comboBox1.Enabled = true;
+                StopButton.Enabled = true;
+                save.Enabled = true;
+                openfile.Enabled = true;
+                return;
+            }
+            timer.Start();
+            TryDraw.Text = "Stop drawing";
+            openfile.Enabled = false;
+            comboBox1.Enabled = false;
+            StopButton.Enabled = false;
+            save.Enabled = false;
+            //drawer.DrawPolygon(new Pen(Color.Red), [new Point(100, 100), new Point(120, 100), new Point(120, 120), new Point(100, 120)]);
+        }
+
+        private void DrawGrid_Click(object sender, EventArgs e)
+        {
+            if (gridflag)
+            {
+                gridflag = true;
+                DrawGrid.Text = "Draw grid";
+                using (Graphics graphics = Graphics.FromImage(map))
+                {
+                    for (int i = 0; i <= count * step; i += step)
+                    {
+                        graphics.DrawLine(new Pen(Brushes.Black, 1), new Point(1, i), new Point(step * count - 1, i));
+                        graphics.DrawLine(new Pen(Brushes.Black, 1), new Point(i, 1), new Point(i, step * count - 1));
+                    }
+                }
+                Picture.Image = map;
+                gridflag = false;
+                return;
+            }
+            gridflag = true;
+            DrawGrid.Text = "Remove grid";
+            using(Graphics graphics = Graphics.FromImage(map))
+            {
+                for (int i = 0; i <= count * step; i += step)
+                {
+                    graphics.DrawLine(new Pen(Brushes.White, 1), new Point(1, i), new Point(step * count - 1, i));
+                    graphics.DrawLine(new Pen(Brushes.White, 1), new Point(i, 1), new Point(i, step * count - 1));
+                }
+            }
+            Picture.Image = map;
+        }
+        private bool Rule(int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= count || y >= count)
+            {
+                return false;
+            }
+            Random random = new Random();
+            int res = random.Next(1000);
+            if(res % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void Draw(int x, int y, int color)
+        {
+            if(x < 0 || y < 0 || x >= count || y >= count)
+            {
+                return;
+            }
+            /*if(StatusGrid[x, y] == 1)
+            {
+                return;
+            }*/
+            //Picture.Image = new Bitmap(count * step, count * step);
+            StatusGrid[x, y] = color;
+            //Graphics.FromImage(map);
+           
+
+            Graphics graphics = Picture.CreateGraphics();
+            //new Point[4] point;
+            
+            for(int i = x * step + 1; i < x * step + step; i++)
+            {
+                for(int j = y * step + 1; j < y * step + step; j++)
+                {
+                    map.SetPixel(i, j, brushes[color]);
+                }
+            }
+            Picture.Image = map;
+            
+            //graphics.FillRectangle(Brushes.Green, x * step + 1, y * step + 1, step - 1, step - 1);
+        }
+
+        private void Picture_MouseClick(object sender, MouseEventArgs e)
+        {
+            Init();
+            termitex = e.X / step;
+            termitey = e.Y / step;
+            //x = x - x % 10;
+            //y = y - y % 10;
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+           
+
+
+                for (int i = 0; i < tickcount; i++)
+                {
+                try
+                {
+
+
+                    var action = rule[new Tuple<char, int>(termitestate, StatusGrid[termitex, termitey])];
+                    Draw(termitex, termitey, action.Item1);
+                    termitestate = action.Item3;
+                    currentdirection = (currentdirection + action.Item2 + 4) % 4;
+                    termitex += directions[currentdirection].Item1;
+                    termitey += directions[currentdirection].Item2;
+                    //Picture.Image = map;
+                }
+                catch
+                {
+                    try
+                    {
+
+
+                        Tuple<int, int> point = new Tuple<int, int>(termitex, termitey);
+                        if (extrapoints.ContainsKey(point))
+                        {
+                            var action = rule[new Tuple<char, int>(termitestate, extrapoints[point])];
+                            extrapoints[point] = action.Item1;
+                            termitestate = action.Item3;
+                            currentdirection = (currentdirection + action.Item2 + 4) % 4;
+                            termitex += directions[currentdirection].Item1;
+                            termitey += directions[currentdirection].Item2;
+                        }
+                        else
+                        {
+                            var action = rule[new Tuple<char, int>(termitestate, 0)];
+                            extrapoints.Add(point, action.Item1);
+                            termitestate = action.Item3;
+                            currentdirection = (currentdirection + action.Item2 + 4) % 4;
+                            termitex += directions[currentdirection].Item1;
+                            termitey += directions[currentdirection].Item2;
+                        }
+                    }
+                    catch
+                    {
+                        //timer.Stop();
+                    }
+                }
+                }
+            
+            
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                var action = rule[new Tuple<char, int>(termitestate, StatusGrid[termitex, termitey])];
+                Draw(termitex, termitey, action.Item1);
+                termitestate = action.Item3;
+                currentdirection = (currentdirection + action.Item2 + 4) % 4;
+                termitex += directions[currentdirection].Item1;
+                termitey += directions[currentdirection].Item2;
+                Picture.Image = map;
+            }
+            catch
+            {
+                try
+                {
+
+
+                    Tuple<int, int> point = new Tuple<int, int>(termitex, termitey);
+                    if (extrapoints.ContainsKey(point))
+                    {
+                        var action = rule[new Tuple<char, int>(termitestate, extrapoints[point])];
+                        extrapoints[point] = action.Item1;
+                        termitestate = action.Item3;
+                        currentdirection = (currentdirection + action.Item2 + 4) % 4;
+                        termitex += directions[currentdirection].Item1;
+                        termitey += directions[currentdirection].Item2;
+                    }
+                    else
+                    {
+                        var action = rule[new Tuple<char, int>(termitestate, 0)];
+                        extrapoints.Add(point, action.Item1);
+                        termitestate = action.Item3;
+                        currentdirection = (currentdirection + action.Item2 + 4) % 4;
+                        termitex += directions[currentdirection].Item1;
+                        termitey += directions[currentdirection].Item2;
+                    }
+                }
+                catch
+                {
+                    timer.Stop();
+                }
+            }
+        }
+
+        private void Ignore_Click(object sender, EventArgs e)
+        {
+            if (ignoreboarderflag)
+            {
+                extrapoints = null;
+                Ignore.Text = "Ignore boarder";
+                ignoreboarderflag = false;
+                return;
+            }
+            
+            Ignore.Text = "Make boarder";
+            ignoreboarderflag = true;
+            extrapoints = new Dictionary<Tuple<int, int>, int>();
+        }
+
+        private void openfile_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Dictionary<Tuple<char, int>, Tuple<int, int, char>> newrule = new Dictionary<Tuple<char, int>, Tuple<int, int, char>>();
+                try
+                {
+                    
+                    using (StreamReader reader = new StreamReader(openFileDialog1.FileName))
+                    {
+                        var data = reader.ReadToEnd();
+                        var lines = data.Split('\n');
+                        foreach(var line in lines){
+                            var line2 = line.Split('\r')[0];
+                            var line1 = line2.Split(' ');
+                            char elem1 = System.Convert.ToChar(line1[0]);
+                            
+                            int elem2 = System.Convert.ToInt32(line1[1]);
+                            int elem3 = System.Convert.ToInt32(line1[2]);
+                            int elem4 = System.Convert.ToInt32(line1[3]);
+                            char elem5 = System.Convert.ToChar(line1[4]);
+                            newrule.Add(new Tuple<char, int>(elem1, elem2), new Tuple<int, int, char>(elem3, elem4, elem5));
+                        }
+                        rule = new Dictionary<Tuple<char, int>, Tuple<int, int, char>>(newrule);
+                        //filename = openFileDialog1.FileName. ;
+                        Init();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Can't parse file into turmite rule\n Please try another file", "OpenFileError", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                map.Save(saveFileDialog1.FileName);
+            }
+            //map.Save(filename + ".bmp");
+        }
+
+        private void Picture_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tickcount = Convert.ToInt32(comboBox2.SelectedItem);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            timer.Stop();
+            step = Convert.ToInt32(comboBox1.SelectedItem);
+            Init();
+        }
+    }
+}
